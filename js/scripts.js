51cd2292e85e5abb032fbbee18decfa6859ad35f
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-23 12:00:22 -0700

    add functions for add,subtract,multiply,divide; tests each

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+// // add
+// var add = function(number1, number2) {
+//   return number1 + number2;
+// };
+
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+
+// // subtract
+// var subtract = function(number1, number2) {
+//   return number1 - number2;
+// };
+
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+
+// // multiply
+// var multiply = function(number1, number2) {
+//   return number1 * number2;
+// };
+
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+
+// divide
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+
+
+
+var result = divide(number1, number2);
+
+alert(result);
