e07cf0b394012992afc9098593895f27428e0357
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-25 10:53:44 -0700

    separate bs logic and ui; stop form from reloading on input

--- js/scripts.js
+++ js/scripts.js
@@ -27,12 +27,17 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
 
 
+// alert(result);
 
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+    // var result = add(number1, number2);
+  })
 
-var result = add(number1, number2);
-
-alert(result);
+});
