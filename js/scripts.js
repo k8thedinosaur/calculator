a44b21343ef8b420bef37e16491b8fc7a55d38c4
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-25 10:03:19 -0700

    add bootstrap, jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // // add
-// var add = function(number1, number2) {
-//   return number1 + number2;
-// };
+var add = function(number1, number2) {
+  return number1 + number2;
+};
 
 // var number1 = parseInt(prompt("Enter a number:"));
 // var number2 = parseInt(prompt("Enter another number:"));
@@ -23,9 +23,9 @@
 // var number2 = parseInt(prompt("Enter another number:"));
 
 // divide
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
+// var divide = function(number1, number2) {
+//   return number1 / number2;
+// };
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
