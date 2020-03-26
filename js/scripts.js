1b5453f1a5b0a270bf0e0f814a27974e5a806ff4
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-26 15:52:39 -0700

    add if/else logic for all operators

--- js/scripts.js
+++ js/scripts.js
@@ -38,9 +38,21 @@ $(document).ready(function() {
     event.preventDefault();
     var number1 = parseInt($("#input1").val());
     var number2 = parseInt($("#input2").val());
-
     var operator = $("input:radio[name=operator]:checked").val();
-    var result = add(number1, number2);
+    // console.log(operator);
+    var result;
+
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+  
+
     $("#output").text(result);
   });
 });
